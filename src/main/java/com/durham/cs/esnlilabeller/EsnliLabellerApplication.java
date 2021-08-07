2743bdd753c6fba88f11bd73da7a66bd0d2c620b
AKJ95 <andyykp@gmail.com>
2021-08-07 12:22:26 +0800

    Initial commit

---
+++ src/main/java/com/durham/cs/esnlilabeller/EsnliLabellerApplication.java
@@ -0,0 +1,13 @@
+package com.durham.cs.esnlilabeller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsnliLabellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsnliLabellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/durham/cs/esnlilabeller/EsnliLabellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durham.cs.esnlilabeller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsnliLabellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
