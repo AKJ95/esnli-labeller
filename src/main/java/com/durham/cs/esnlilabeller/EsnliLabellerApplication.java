a88471cad6470cd335f7731cf8233355d99b79d4
AKJ95 <andyykp@gmail.com>
2021-08-07 13:13:19 +0800

    Completed backend configurations.

--- src/main/java/com/durham/cs/esnlilabeller/EsnliLabellerApplication.java
+++ src/main/java/com/durham/cs/esnlilabeller/EsnliLabellerApplication.java
@@ -1,13 +1,16 @@
 package com.durham.cs.esnlilabeller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EsnliLabellerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EsnliLabellerApplication.class, args);
+		log.info("Swagger Document: http://localhost:8080/swagger-ui.html#/");
 	}
 
 }
